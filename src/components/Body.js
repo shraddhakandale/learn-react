b49ffb8d76c7386106f7018790a51dbc666dfeb0
shraddhakandale <sorkeyy@gmail.com>
2025-06-10 23:17:50 +0530

    Episode-6 upto part 4

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,26 +1,39 @@
 import RestaurantCard from "./RestaurantCard";
-import resObj from "../utils/mockData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Body = () => {
-  const [resList, setresList] = useState(resObj);
-  const [searchText, setsearchText] = useState("");
+  const [resList, setresList] = useState([]);
+
+  useEffect(()=>{
+    fetchData();
+  },[])
+
+  const fetchData = async ()=>{
+    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.5904779&lng=73.7271909&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+    const json = await data.json();
+    // optional chaining
+    setresList(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+    console.log((json.data.cards[1]))
+    console.log(json);
+  };
+
+  if(resList.length === 0){
+    return (
+        <h1>Loading...</h1>
+    )
+  }
 
   return (
     <div>
       <div className="body">
         <div className="searchbar">
-          <input type="text" value={searchText} onChange={(e)=>{
-            setsearchText(e.target.value)
-          }}/>
         </div>
         <div className="filter-btn">
           <button
             onClick={() => {
               const filteredList = resList.filter(res =>
-                res.info.name.toLocaleLowerCase().includes(searchText)
+                res.info.avgRating > 4
               );
-              console.log(searchText);
               setresList(filteredList);
             }}
           >

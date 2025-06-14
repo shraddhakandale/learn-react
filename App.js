47eafe1437a8e3222fcde1de22c93b50f00ddfcd
shraddhakandale <sorkeyy@gmail.com>
2025-06-14 20:59:28 +0530

    Episode 7 router concepts.

--- App.js
+++ App.js
@@ -3,17 +3,43 @@ import ReactDOM from 'react-dom/client'
 import Header from "./src/components/Header";
 import Body from "./src/components/Body";
 import { CDN_URL } from "./src/utils/constants";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router";
+import About from "./src/components/About";
+import Contact from "./src/components/Contact";
+import Error from "./src/components/Error";
 
 const App = () => {
     return (
         <div>
             <Header />
-            <Body />
+            <Outlet />
         </div>
     )
 }
 
+const router = createBrowserRouter([
+    {
+        path:"/",
+        element: <App/>,
+        children: [
+            {
+                path: "/",
+                element: <Body />
+            },
+            {
+                path:"/about",
+                element: <About/>
+            },
+            {
+                path:"/contact",
+                element: <Contact/>
+            }
+        ],
+        errorElement: <Error/>
+    },
+])
+
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
-    <App />
+    <RouterProvider router={router} />
 )
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,7 @@
 import { LOGO_URL } from "../utils/constants";
 import { useState } from "react";
+import { Link } from "react-router";
+
 const Header = () => {
     const [btnName, setbtnName] = useState("Login")
     return (
@@ -10,9 +12,9 @@ const Header = () => {
             </div>
             <div className="navbar">
                 <ul>
-                    <li>Home</li>
-                    <li>About Us</li>
-                    <li>Contact Us</li>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/about">About Us</Link></li>
+                    <li><Link to="/contact">Contact Us</Link></li>
                     <li>Cart</li>
                     <button onClick={()=>{
                         btnName === "Login" ? setbtnName("Logout") : setbtnName("Login")

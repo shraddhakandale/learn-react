65c63d08b078e8eb4219665e0bc92f838000aa83
shraddhakandale <sorkeyy@gmail.com>
2025-06-19 21:20:17 +0530

    class component lifecycle methods.

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import UserClass from "./UserClass";
 const About = () => {
   return (
     <div>
-      <User name={"Shraddha function prop"} />
+      {/* <User name={"Shraddha function prop"} /> */}
       <UserClass name={"Shraddha class prop"} />
     </div>
   );
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -4,25 +4,39 @@ class UserClass extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            count:0,
-            count2:1
+            user:{
+                name:"null",
+                location:"test"
+            }
         };
     }
+
+    async componentDidMount(){
+        console.log("component did mount")
+        const data = await fetch("https://api.github.com/users/shraddhakandale");
+        const json = await data.json()
+        this.setState({
+            user:{
+                name: json.name,
+                location: json.location
+            }
+        })
+    }
+
+    componentDidUpdate(){
+        console.log("component update")
+    }
+
+    componentWillUnmount(){
+        console.log("component will unmount")
+    }
+
     render(){
-        const {name} = this.props;
-        const{count, count2} = this.state;
-        
+        const {name, location} = this.state.user;
         return(
             <div className="user-card">
-                <h2>Count: {count}</h2>
-                <button onClick={()=>{
-                    this.setState({
-                        count: this.state.count + 1
-                    })
-                }}>Increment</button>
-                <h2>Count2: {count2}</h2>
                 <h2>Name: {name}</h2>
-                <h2>Location: Pune</h2>
+                <h2>Location: {location}</h2>
                 <h2>Contact: @sorkeyy</h2>
             </div>
         )
